b38d7681bb609c28b9d06e1ba9d0cfbd74276859
Jusley Souza <jusleysouza31@gmail.com>
2022-12-13 14:07:22 -0300

    Creation of the findByNameAndZoneId method using paging

--- src/main/java/com/ju/eventmanagement/repository/EventRepository.java
+++ src/main/java/com/ju/eventmanagement/repository/EventRepository.java
@@ -1,7 +1,9 @@
 package com.ju.eventmanagement.repository;
 
-import java.util.List;
+import java.time.ZoneId;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -9,6 +11,8 @@ import com.ju.eventmanagement.entities.Event;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
-	List<Event> findByName(@Param("name") String name);
+	Page<Event> findByName(@Param("name") String name, Pageable pageable);
+	
+	Page<Event> findByNameAndZoneId(@Param("name") String name, @Param("zoneId") ZoneId zoneId, Pageable pageable );
 	
 }
