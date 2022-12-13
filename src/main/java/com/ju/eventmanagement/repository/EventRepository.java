bc208df0ab486d61e387b5f0e638290fe129f505
Jusley Souza <jusleysouza31@gmail.com>
2022-12-13 13:55:17 -0300

    Creation of the findByName method using paging

--- src/main/java/com/ju/eventmanagement/repository/EventRepository.java
+++ src/main/java/com/ju/eventmanagement/repository/EventRepository.java
@@ -1,9 +1,14 @@
 package com.ju.eventmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ju.eventmanagement.entities.Event;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
+	List<Event> findByName(@Param("name") String name);
+	
 }
