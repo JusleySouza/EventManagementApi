bac7d982a756d6582b7e8159aeda71ad303addaa
Jusley Souza <jusleysouza31@gmail.com>
2022-12-13 11:21:13 -0300

    Creation and implementation of the Organizer repository

---
+++ src/main/java/com/ju/eventmanagement/repository/OrganizerRepository.java
@@ -0,0 +1,9 @@
+package com.ju.eventmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ju.eventmanagement.entities.Organizer;
+
+public interface OrganizerRepository extends CrudRepository<Organizer, Long> {
+
+}
