faa586aaed698d8c7c032111d69a89004df99712
Jusley Souza <jusleysouza31@gmail.com>
2022-12-13 11:21:57 -0300

    Creation and implementation of the Venue repository

---
+++ src/main/java/com/ju/eventmanagement/repository/VenueRepository.java
@@ -0,0 +1,9 @@
+package com.ju.eventmanagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ju.eventmanagement.entities.Venue;
+
+public interface VenueRepository extends CrudRepository<Venue, Long> {
+
+}
