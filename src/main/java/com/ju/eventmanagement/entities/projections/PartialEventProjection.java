bd913c63104ca7e99868b95d08c218f420bbfc35
Jusley Souza <jusleysouza31@gmail.com>
2022-12-14 13:29:40 -0300

    Creation and Implementation of the PartialEventProjection class

---
+++ src/main/java/com/ju/eventmanagement/entities/projections/PartialEventProjection.java
@@ -0,0 +1,21 @@
+package com.ju.eventmanagement.entities.projections;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import com.ju.eventmanagement.entities.Event;
+
+@Projection(name = "partial", types = { Event.class })
+public interface PartialEventProjection {
+
+	String getName();
+
+	ZonedDateTime getStartTime();
+
+	ZonedDateTime getEndTime();
+	
+	Instant getCreated();
+
+}
