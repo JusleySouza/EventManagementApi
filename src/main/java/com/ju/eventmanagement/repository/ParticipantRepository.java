3fc6bccfa0cc96f3d045ff4dc475940dc5e708ff
Jusley Souza <jusleysouza31@gmail.com>
2022-12-14 10:51:16 -0300

    Creation of the findByIdAndEmail method using paging

--- src/main/java/com/ju/eventmanagement/repository/ParticipantRepository.java
+++ src/main/java/com/ju/eventmanagement/repository/ParticipantRepository.java
@@ -1,9 +1,14 @@
 package com.ju.eventmanagement.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ju.eventmanagement.entities.Participant;
 
 public interface ParticipantRepository extends PagingAndSortingRepository<Participant, Long> {
 
+	Page<Participant> findByIdAndEmail(@Param("id") Long id, @Param("email") String email, Pageable pageable);
+	
 }
